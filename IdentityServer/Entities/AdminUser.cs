de319ce36b7cbce7e284442036ac85062b0031ce
returner <returner@daum.net>
2022-06-08 15:42:57 +0900

    add AdminUser model

---
+++ IdentityServer/Entities/AdminUser.cs
@@ -0,0 +1,12 @@
+﻿namespace Identity.Entities
+{
+    public record AdminUser
+    {
+        public int Id { get; set; }
+        public string? UserId { get; set; }
+        public string? Password { get; set; }
+        public string? Email { get; set; }
+        public string? Name { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
---
+++ IdentityServer/Entities/IdentityContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Identity.Entities
+{
+    public class IdentityContext : DbContext
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<AdminUser> AdminUsers { get; set; } = null!;
+    }
+
+    
+}
